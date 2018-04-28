a6f32a26c04c19aa5ef4adc4e6b140eb7ff5c700
dengjuan <296930397@qq.com>
2018-04-28 11:15:33 +0800

    login

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,15 @@
 import Login from './logintest.vue'
 
+
+ const login = function(Vue) {
+    Vue.component(Login.name, Login)
+  }
+
+// 这里的判断很重要
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(comment)
+}
+
 export {
-  Login
+  login
 }
